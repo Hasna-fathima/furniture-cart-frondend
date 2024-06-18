4519f57d0a399e404cc25c810d4f3b194b2a1106
Hasna-fathima <160711400+Hasna-fathima@users.noreply.github.com>
2024-06-17 22:15:15 -0700

    homa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import Topcategories from './Topcategories';
-
+import '../index.css'
 
 function Home() {
   return (
-    <div className="container"> 
-
-
-    <div className='contentcontainer'>
-      <h2>Modern Interior Design Studio</h2>
-      <p>The Distinct Style Focus On Simple Form And Function, <br/>Which Are Available As Equals Under This Style</p>
-       <button>Shop Now</button>
+    <div className='container3'>
+      <div className="home"> 
+        <div className='contentcontainer'>
+          <h2>Modern Interior Design Studio</h2>
+          <p>The Distinct Style Focus On Simple Form And Function, <br/>Which Are Available As Equals Under This Style</p>
+          <button>Shop Now</button>
+        </div>
+        <div className='imagecontainer'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqzNSt_lVww64HErsIVqCW8gW-_0EAdhSTcBXSfmaZ-77fErKUh_6_54U4mh-LcR9IJzs&usqp=CAU" alt="Modern Interior Design" />
+        </div>  
+      </div>
     </div>
-    <div className='imagecontainer'>
-      
-    </div>
-    <Topcategories/>
-
-      
-     </div>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
