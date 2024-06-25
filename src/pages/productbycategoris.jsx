2777c8e4c12fec1f314a1ba70f59acc8ba092283
Hasna-fathima <160711400+Hasna-fathima@users.noreply.github.com>
2024-06-25 03:35:50 -0700

    api edit

--- src/pages/productbycategoris.jsx
+++ src/pages/productbycategoris.jsx
@@ -10,7 +10,7 @@ function ProductsByCategory() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get(`https://furniture-cart-5.onrender.com/user/products?category=${category}`);
+        const response = await axios.get(`https://furniture-cart-5.onrender.com/api/user/products?category=${category}`);
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
